1420cede4c4dbfd5318536a67dbbea25c5aa28d1
mayasinama <114096128+mayasinama@users.noreply.github.com>
2022-11-24 00:57:24 +0000

    update

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const edgeChromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const executablePath = await edgeChromium.executablePath
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: true,
+  })
+
+  const [page] = await browser.pages();
+
+  await page.goto('https://animixplay.to/v1/do-it-yourself/ep8', { waitUntil: 'networkidle0' });
+
+  const data = await page.content()
+
+  await browser.close();
+  
+  res.send(data)
 })
 
 app.listen(port, () => {
